7733fc9cfa0b3b8a97bcf712885889ac10f115a4
RevanReborn <smithmaxmax@ukr.net>
2018-05-29 00:08:21 +0300

    placeholder login; database display; UI touch ups

--- kuratorDB/Form1.Designer.cs
+++ kuratorDB/Form1.Designer.cs
@@ -29,12 +29,296 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dUT_databaseDataSet = new kuratorDB.DUT_databaseDataSet();
+            this.dUTdatabaseDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.студентиBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.студентиTableAdapter = new kuratorDB.DUT_databaseDataSetTableAdapters.СтудентиTableAdapter();
+            this.tableAdapterManager = new kuratorDB.DUT_databaseDataSetTableAdapters.TableAdapterManager();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dUT_databaseDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dUTdatabaseDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.студентиBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(844, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loginToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(53, 20);
+            this.toolStripMenuItem1.Text = "Меню";
+            // 
+            // loginToolStripMenuItem
+            // 
+            this.loginToolStripMenuItem.Name = "loginToolStripMenuItem";
+            this.loginToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.loginToolStripMenuItem.Text = "Logout";
+            this.loginToolStripMenuItem.Click += new System.EventHandler(this.loginToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(36, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(36, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(82, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(305, 153);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(199, 169);
+            this.panel1.TabIndex = 6;
+            // 
+            // dUT_databaseDataSet
+            // 
+            this.dUT_databaseDataSet.DataSetName = "DUT_databaseDataSet";
+            this.dUT_databaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dUTdatabaseDataSetBindingSource
+            // 
+            this.dUTdatabaseDataSetBindingSource.DataSource = this.dUT_databaseDataSet;
+            this.dUTdatabaseDataSetBindingSource.Position = 0;
+            // 
+            // студентиBindingSource
+            // 
+            this.студентиBindingSource.DataMember = "Студенти";
+            this.студентиBindingSource.DataSource = this.dUT_databaseDataSet;
+            // 
+            // студентиTableAdapter
+            // 
+            this.студентиTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.UpdateOrder = kuratorDB.DUT_databaseDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            this.tableAdapterManager.ВикладачіTableAdapter = null;
+            this.tableAdapterManager.ГрупиTableAdapter = null;
+            this.tableAdapterManager.ДисципліниTableAdapter = null;
+            this.tableAdapterManager.Загальні_виховні_заходиTableAdapter = null;
+            this.tableAdapterManager.ЗалікиTableAdapter = null;
+            this.tableAdapterManager.Індивідуальна_робота_куратора_зі_студентами_та_батькамиTableAdapter = null;
+            this.tableAdapterManager.КафедриTableAdapter = null;
+            this.tableAdapterManager.Кураторська_роботаTableAdapter = null;
+            this.tableAdapterManager.МістоTableAdapter = null;
+            this.tableAdapterManager.МісяціTableAdapter = null;
+            this.tableAdapterManager.Оплата_за_навчання_студентами_контрактної_форми_навчанняTableAdapter = null;
+            this.tableAdapterManager.Оплата_за_проживання_в_гуртожиткуTableAdapter = null;
+            this.tableAdapterManager.ОцінкаTableAdapter = null;
+            this.tableAdapterManager.Підсумки_роботи_за_навчальний_рікTableAdapter = null;
+            this.tableAdapterManager.Пільги_у_студентівTableAdapter = null;
+            this.tableAdapterManager.ПільгиTableAdapter = null;
+            this.tableAdapterManager.План_роботи_куратораTableAdapter = null;
+            this.tableAdapterManager.СпеціальностіTableAdapter = null;
+            this.tableAdapterManager.СтудентиTableAdapter = this.студентиTableAdapter;
+            this.tableAdapterManager.УніверситетиTableAdapter = null;
+            this.tableAdapterManager.ФакультетиTableAdapter = null;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(218, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(614, 446);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.Visible = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.comboBox1);
+            this.panel2.Location = new System.Drawing.Point(12, 27);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 446);
+            this.panel2.TabIndex = 8;
+            this.panel2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Таблиці";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(3, 16);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(194, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 476);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(844, 22);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(77, 17);
+            this.toolStripStatusLabel1.Text = "Logged in as:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(844, 498);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Система управління автоматизацією процесу роботи куратора";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dUT_databaseDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dUTdatabaseDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.студентиBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.BindingSource dUTdatabaseDataSetBindingSource;
+        private DUT_databaseDataSet dUT_databaseDataSet;
+        private System.Windows.Forms.BindingSource студентиBindingSource;
+        private DUT_databaseDataSetTableAdapters.СтудентиTableAdapter студентиTableAdapter;
+        private DUT_databaseDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
 
--- kuratorDB/Form1.cs
+++ kuratorDB/Form1.cs
@@ -6,14 +6,104 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Data.OleDb;
 
 namespace kuratorDB
 {
     public partial class Form1 : Form
     {
+        static string CONNECT_STR = string.Format("Provider = Microsoft.ACE.OLEDB.12.0; Data Source={0}",
+            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DUT_database.accdb"));
+
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //OleDbConnection con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=DUT_database.accdb");
+            //
+            comboBox1.Items.Clear();
+            OleDbConnection dbCon = new OleDbConnection(CONNECT_STR);
+            dbCon.Open();
+            DataTable tbls = dbCon.GetSchema("Tables", new string[] { null, null, null, "TABLE" }); //список всех таблиц
+            foreach (DataRow row in tbls.Rows)
+            {
+                string TableName = row["TABLE_NAME"].ToString();
+                comboBox1.Items.Add(TableName);
+            }
+            dbCon.Close();
+            //
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            interfaceChangeOnLogin();
+            
+        }
+
+        private void interfaceChangeOnLogin()
+        {
+            panel1.Visible = false;
+            panel2.Visible = true;
+            //panel2.Dock = DockStyle.Left;
+            //panel3.Visible = true;
+            //panel3.Dock = DockStyle.Bottom;
+            dataGridView1.Visible = true;
+            //dataGridView1.Dock = DockStyle.Fill;
+        }
+
+        private void interfaceChangeOnLogout()
+        {
+            panel1.Visible = true;
+            panel2.Visible = false;
+            //panel3.Visible = false;
+            dataGridView1.Visible = false;
+        }
+
+        private void loginToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            interfaceChangeOnLogout();
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            UpdateView(comboBox1.Text);
+        }
+
+        private void UpdateView(string tableName)
+        {
+            try
+            {
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = GetTableByName(tableName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        public static DataTable GetTableByName(string tableName)
+        {
+            DataTable dt = new DataTable(tableName);
+            string command = string.Format("SELECT * FROM {0}", tableName);
+            using (OleDbConnection cnn = new OleDbConnection(CONNECT_STR))
+            {
+                cnn.Open();
+                using (OleDbCommand cmd = new OleDbCommand(command, cnn))
+                {
+                    using (OleDbDataReader dr = cmd.ExecuteReader())
+                    {
+                        dt.Load(dr);
+                    }
+                }
+            }
+            return dt;
         }
     }
 }
